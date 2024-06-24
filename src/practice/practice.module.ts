5dd91572ce97c593836558dcd26c2beec01bbed9
Akadil <akadil.kalimoldayev@gmail.com>
2024-06-24 17:13:33 +0200

    Cleaning

--- src/practice/practice.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PracticeService } from './practice.service';
-import { PracticeController } from './practice.controller';
-
-@Module({
-    controllers: [PracticeController],
-    providers: [PracticeService],
-})
-export class PracticeModule {}
